d3cb2fb1679f7fa8116741ad39babfda3b56a0da
Gilberto Xavier <93664041+gilbertosxavier@users.noreply.github.com>
2025-01-07 11:11:04 -0300

    Exercícios
    Criados exercícios 8 e 9 utilizando HTML, CSS e JavaScript.

---
+++ módulo D/ex008.js
@@ -0,0 +1,7 @@
+let = vel = 70
+console.log(`Sua velocidade é ${vel}km/h`)
+
+if(vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida na via, portanto será multado!`)
+}
+console.log(`Diriga com cuidado e use sempre o cinto de segurança!`)
---
+++ módulo D/ex009.js
@@ -0,0 +1,7 @@
+let country = 'Brasil'
+
+if (country != 'Brasil') {
+    console.log(`Você é estrangeiro!`)
+}else {
+    console.log(`Você é Brasileiro!`)
+}
